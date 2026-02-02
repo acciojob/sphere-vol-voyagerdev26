97bf01cdbb584a6df6c8fdc1242c03f80c11d07a
Dev Sharma <103065378+voyagerdev26@users.noreply.github.com>
2026-02-03 00:56:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function volume_sphere() {
+let form = document.getElementById("MyForm");
+form.addEventListener("submit",volume_sphere);
+
+function volume_sphere(e) {
     //Write your code here
-  
+	e.preventDefault();
+	let valR= +document.getElementById("radius").value;
+	if(valR==NaN || valR<0){
+		document.getElementById("volume").value="NaN";
+	}
+	else{
+		document.getElementById("volume").value=((4/3)*3.14*(valR**3)).toFixed(4);
+	}
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
